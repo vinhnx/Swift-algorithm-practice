59b50a307f43fc8f9b75bc631266fae5a6cfa606
Vinh Nguyen <vinhnguyen2308@gmail.com>
2019-08-05 15:15:35 +0700

    Binary Tree

--- algo_practice.playground/Contents.swift
+++ algo_practice.playground/Contents.swift
@@ -392,3 +392,79 @@ sodaNode.addChild(bitterLemonNode)
 tree.search("cocoa") // returns the "cocoa" node
 tree.search("chai") // returns the "chai" node
 tree.search("bubbly") // nil
+
+// binary tree
+// > a `tree` where each node has 0, 1 or 2 children
+// https://github.com/raywenderlich/swift-algorithm-club/tree/master/Binary%20Tree
+indirect enum BinaryTree<T> {
+    case node(BinaryTree<T>, T, BinaryTree<T>)
+    case empty
+}
+
+extension BinaryTree {
+    var count: Int {
+        switch self {
+        case let .node(left, _, right):
+            return left.count + 1 + right.count
+        case .empty:
+            return 0
+        }
+    }
+
+    func traverseInOrder(_ handler: (T) -> Void) {
+        if case let .node(left, current, right) = self {
+            left.traverseInOrder(handler)
+            handler(current)
+            right.traverseInOrder(handler)
+        }
+    }
+
+    func traversePreOrder(_ handler: (T) -> Void) {
+        if case let .node(left, current, right) = self {
+            handler(current)
+            left.traversePreOrder(handler)
+            right.traversePreOrder(handler)
+        }
+    }
+
+    func traversePostOrder(_ handler: (T) -> Void) {
+        if case let .node(left, current, right) = self {
+            left.traversePostOrder(handler)
+            right.traversePostOrder(handler)
+            handler(current)
+        }
+    }
+}
+
+extension BinaryTree: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case let .node(left, value, right):
+            return "value: \(value), left = [\(left.description)], right = [\(right.description)]"
+        case .empty:
+            return ""
+        }
+    }
+}
+
+// leaf nodes
+let node5 = BinaryTree.node(.empty, "5", .empty)
+let nodeA = BinaryTree.node(.empty, "a", .empty)
+let node10 = BinaryTree.node(.empty, "10", .empty)
+let node4 = BinaryTree.node(.empty, "4", .empty)
+let node3 = BinaryTree.node(.empty, "3", .empty)
+let nodeB = BinaryTree.node(.empty, "b", .empty)
+
+// intermediate nodes on the left
+let Aminus10 = BinaryTree.node(nodeA, "-", node10)
+let timesLeft = BinaryTree.node(node5, "*", Aminus10)
+
+// intermediate nodes on the right
+let minus4 = BinaryTree.node(.empty, "-", node4)
+let divide3andB = BinaryTree.node(node3, "/", nodeB)
+let timesRight = BinaryTree.node(minus4, "*", divide3andB)
+
+// root node
+let binaryTree = BinaryTree.node(timesLeft, "+", timesRight)
+binaryTree.count
+print(binaryTree)
