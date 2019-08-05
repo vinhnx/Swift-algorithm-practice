6fcb031a7277fa670ad38991d17d55b3ae0cfab0
Vinh Nguyen <vinhnguyen2308@gmail.com>
2019-08-05 14:58:17 +0700

    Tree

--- algo_practice.playground/Contents.swift
+++ algo_practice.playground/Contents.swift
@@ -318,3 +318,77 @@ class EdgeList<T> where T: Equatable & Hashable {
     }
 }
 
+// tree
+// https://github.com/raywenderlich/swift-algorithm-club/tree/master/Tree
+class TreeNode<T> {
+    var value: T
+    weak var parent: TreeNode?
+    var children = [TreeNode<T>]()
+
+    init(value: T) {
+        self.value = value
+    }
+
+    func addChild(_ node: TreeNode<T>) {
+        children.append(node)
+        node.parent = self
+    }
+}
+
+extension TreeNode: CustomStringConvertible {
+    public var description: String {
+        var s = "\(value)"
+        if !children.isEmpty {
+            s += " {" + children.map { $0.description }.joined(separator: ", ") + "}"
+        }
+        return s
+    }
+}
+
+extension TreeNode where T: Equatable {
+    func search(_ value: T) -> TreeNode? {
+        if value == self.value { return self }
+
+        for child in children {
+            guard let result = child.search(value) else { continue }
+            return result
+        }
+
+        return nil
+    }
+}
+
+let tree = TreeNode<String>(value: "beverages")
+let hotNode = TreeNode<String>(value: "hot")
+let coldNode = TreeNode<String>(value: "cold")
+let teaNode = TreeNode<String>(value: "tea")
+let coffeeNode = TreeNode<String>(value: "coffee")
+let chocolateNode = TreeNode<String>(value: "cocoa")
+let blackTeaNode = TreeNode<String>(value: "black")
+let greenTeaNode = TreeNode<String>(value: "green")
+let chaiTeaNode = TreeNode<String>(value: "chai")
+let sodaNode = TreeNode<String>(value: "soda")
+let milkNode = TreeNode<String>(value: "milk")
+let gingerAleNode = TreeNode<String>(value: "ginger ale")
+let bitterLemonNode = TreeNode<String>(value: "bitter lemon")
+
+tree.addChild(hotNode)
+tree.addChild(coldNode)
+
+hotNode.addChild(teaNode)
+hotNode.addChild(coffeeNode)
+hotNode.addChild(chocolateNode)
+
+coldNode.addChild(sodaNode)
+coldNode.addChild(milkNode)
+
+teaNode.addChild(blackTeaNode)
+teaNode.addChild(greenTeaNode)
+teaNode.addChild(chaiTeaNode)
+
+sodaNode.addChild(gingerAleNode)
+sodaNode.addChild(bitterLemonNode)
+
+tree.search("cocoa") // returns the "cocoa" node
+tree.search("chai") // returns the "chai" node
+tree.search("bubbly") // nil
